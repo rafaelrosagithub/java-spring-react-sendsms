987eb0fc72c60e75c606ca0d18f8097b7410d66b
Rafael <rafael_unis@hotmail.com>
2022-09-07 14:50:08 -0300

    Created project

---
+++ backend/src/main/java/com/rafael/projectsendsms/ProjectsendsmsApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.projectsendsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsendsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsendsmsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rafael/projectsendsms/ProjectsendsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.projectsendsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsendsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
